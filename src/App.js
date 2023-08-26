cb06d10e6990d9b5f65bf91e16a78334c6c05a83
Danil Vorontsov <rojerr@MacBook-Air-Danil.local>
2023-08-26 17:55:03 +0600

    modal

--- src/App.js
+++ src/App.js
@@ -1,20 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './index.scss';
 
 function App() {
+  const [window, setWindow] = useState(false);
+
+  const openWindow = () => {
+    setWindow(true);
+  };
+
+  const closeWindow = () => {
+    setWindow(false);
+  };
+
   return (
-    <div className="App">
-      <button className="open-modal-btn">✨ Открыть окно</button>
-      {/* <div className="overlay">
-        <div className="modal">
-          <svg height="200" viewBox="0 0 200 200" width="200">
-            <title />
-            <path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z" />
-          </svg>
-          <img src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif" />
+    <>
+      <div className="App">
+        <button onClick={openWindow} className="open-modal-btn">
+          ✨ Открыть окно
+        </button>
+
+        <div className={`overlay animated ${window ? 'show' : ''}`}>
+          <div className="modal">
+            <svg onClick={closeWindow} height="200" viewBox="0 0 200 200" width="200">
+              <title />
+              <path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z" />
+            </svg>
+            <img src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif" />
+          </div>
         </div>
-      </div> */}
-    </div>
+      </div>
+    </>
   );
 }
 
